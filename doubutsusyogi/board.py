acbd01457cb2acdf218940cd2e57a6da344b6b3c
Otani Naoki <o.naoki0302@gmail.com>
2023-10-20 13:15:08 +0900

    feat: implemented search

--- doubutsusyogi/board.py
+++ doubutsusyogi/board.py
@@ -5,6 +5,7 @@
 をぞう、きりんでも表現することで、6bit、Bも同様で12bitで表現可能
 A0 B0 C0 ... C3, Aのひよこ個数, Aのぞう個数, Aのきりん個数, Bのひよこ個数, Bのぞう個数, Bのきりん個数
 結果、60bitで局面が表現される。
+1bit終局フラグをつける。1なら終局、0なら終局していない。
 
   Player B
     0  1  2 x
@@ -16,7 +17,7 @@ y
   Player A
 """
 
-from typing import Set
+from typing import List
 
 EMPTY = 0
 A_HIYOKO = 1
@@ -29,6 +30,8 @@ B_ZOU = 7
 B_KIRIN = 8
 B_LION = 9
 B_NIWATORI = 10
+
+INVALID_BOARD = 0xFFFFFFFFFFFFFFFF
 
 
 def A_hiyoko(board: int) -> int:
@@ -79,6 +82,14 @@ def A_kirin_dec(board: int) -> int:
     return board - (1 << 52)
 
 
+def get_gameover(board: int) -> bool:
+    return (board >> 60) & 0b1 == 1
+
+
+def set_gameover(board: int) -> int:
+    return board | (1 << 60)
+
+
 def get_board_cell(board: int, x: int, y: int) -> int:
     return (board >> (4 * (x + 3 * y))) & 0b1111
 
@@ -95,13 +106,13 @@ def set_board_celli(board: int, i: int, piece: int) -> int:
     return board | (piece << (4 * i))
 
 
-def get_next_boards(board: int) -> Set[int]:
+def get_next_boards(board: int) -> List[int]:
     """プレイヤーAの手番で、boardから次の局面を生成する
 
     Returns:
         list[int]: 次の局面(プレイヤーBの手番) のリスト
     """
-    ret: Set[int] = []
+    ret: List[int] = []
     for i in range(12):
         piece = get_board_celli(board, i)
         if piece == EMPTY:
@@ -119,28 +130,124 @@ def get_next_boards(board: int) -> Set[int]:
     return ret
 
 
-def get_next_boards_empty(board: int, i: int) -> Set[int]:
+def get_next_boards_empty(board: int, i: int) -> List[int]:
     """Aがiの場所に駒を置くときの局面を考える"""
-    ret = set()
+    ret = []
     if A_hiyoko(board) > 0:
         new_board = set_board_celli(board, i, A_HIYOKO)
         new_board = A_hiyoko_dec(new_board)
-        ret.add(new_board)
+        ret.append(new_board)
     if A_zou(board) > 0:
         new_board = set_board_celli(board, i, A_ZOU)
         new_board = A_zou_dec(new_board)
-        ret.add(new_board)
+        ret.append(new_board)
     if A_kirin(board) > 0:
         new_board = set_board_celli(board, i, A_KIRIN)
         new_board = A_kirin_dec(new_board)
-        ret.add(new_board)
+        ret.append(new_board)
     return ret
 
 
-def get_next_boards_hiyoko(board: int, i: int) -> Set[int]:
+def get_next_boards_hiyoko(board: int, i: int) -> List[int]:
     """Aがiの場所のひよこを動かすときの局面を考える"""
-    ret = set()
+    ret: List[int] = []
+    if i < 3:
+        return ret  # no move
+
+    next_pos = i - 3
+    new_board = move(board, i, next_pos)
+    if new_board != INVALID_BOARD:
+        ret.append(new_board)
     return ret
+
+
+def get_next_boards_zou(board: int, i: int) -> List[int]:
+    ret = []
+    x, y = i % 3, i // 3
+    for dx, dy in [(1, 1), (1, -1), (-1, 1), (-1, -1)]:
+        if in_board(x + dx, y + dy):
+            next_pos = x + dx + 3 * (y + dy)
+            new_board = move(board, i, next_pos)
+            if new_board != INVALID_BOARD:
+                ret.append(new_board)
+    return ret
+
+
+def get_next_boards_kirin(board: int, i: int) -> List[int]:
+    ret = []
+    x, y = i % 3, i // 3
+    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        if in_board(x + dx, y + dy):
+            next_pos = x + dx + 3 * (y + dy)
+            new_board = move(board, i, next_pos)
+            if new_board != INVALID_BOARD:
+                ret.append(new_board)
+    return ret
+
+
+def get_next_boards_lion(board: int, i: int) -> List[int]:
+    ret = []
+    x, y = i % 3, i // 3
+    for dx, dy in [
+        (1, 0),
+        (-1, 0),
+        (0, 1),
+        (0, -1),
+        (1, 1),
+        (1, -1),
+        (-1, 1),
+        (-1, -1),
+    ]:
+        if in_board(x + dx, y + dy):
+            next_pos = x + dx + 3 * (y + dy)
+            new_board = move(board, i, next_pos)
+            if new_board != INVALID_BOARD:
+                ret.append(new_board)
+    return ret
+
+
+def get_next_boards_niwatori(board: int, i: int) -> List[int]:
+    ret = []
+    x, y = i % 3, i // 3
+    for dx, dy in [(1, 1), (1, -1), (1, 0), (-1, 0), (0, 1), (0, -1)]:
+        if in_board(x + dx, y + dy):
+            next_pos = x + dx + 3 * (y + dy)
+            new_board = move(board, i, next_pos)
+            if new_board != INVALID_BOARD:
+                ret.append(new_board)
+    return ret
+
+
+def in_board(x: int, y: int) -> bool:
+    return x >= 0 and x < 3 and y >= 0 and y < 4
+
+
+def move(board, move_from_i, move_to_i) -> int:
+    """Aがboardのmove_from_iの駒をmove_to_iに移動する
+    駒は場外に出ることはないとする
+    移動先が移動できない場所の場合はINVALID_BOARDを返す
+    """
+    from_piece = get_board_celli(board, move_from_i)
+    next_piece = get_board_celli(board, move_to_i)
+    new_board = set_board_celli(board, move_from_i, EMPTY)
+    new_board = set_board_celli(new_board, move_to_i, from_piece)
+    # トライで勝ちフラグ
+    if from_piece == A_LION and move_to_i < 3:
+        new_board = set_gameover(new_board)
+    if next_piece == EMPTY:
+        return new_board
+    elif next_piece == B_HIYOKO or next_piece == B_NIWATORI:
+        return A_hiyoko_inc(new_board)
+    elif next_piece == B_ZOU:
+        return A_zou_inc(new_board)
+    elif next_piece == B_KIRIN:
+        return A_kirin_inc(new_board)
+    elif next_piece == B_LION:
+        # キャッチで勝ちフラグ
+        new_board = set_gameover(new_board)
+        return new_board
+    else:  # 自分の駒があるところには移動できない
+        return INVALID_BOARD
 
 
 def board_flip(board: int) -> int:
--- doubutsusyogi/search.py
+++ doubutsusyogi/search.py
@@ -2,7 +2,12 @@ import pickle
 from collections import deque
 from typing import Dict
 
-from doubutsusyogi.board import board_flip, get_first_board, get_next_boards
+from doubutsusyogi.board import (
+    board_flip,
+    get_first_board,
+    get_gameover,
+    get_next_boards,
+)
 
 
 def search():
@@ -12,15 +17,15 @@ def search():
     queue = deque([first_board])
     dist: int = 0
     while len(queue) != 0:
-        print(dist)
-        print(len(queue))
+        # print(len(queue))
         board = queue.popleft()
         for next in get_next_boards(board):
             next = board_flip(next)
             if next not in visited_dict:
                 visited_dict[next] = True
-                queue.append(next)
-        dist += 1
+                if not get_gameover(next):
+                    queue.append(next)
+        # dist += 1
     with open("visited_dict.pickle", "wb") as f:
         pickle.dump(visited_dict, f)
 
